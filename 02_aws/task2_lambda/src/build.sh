9f5574e975dd94e99ba68ac6a6604f668c61f76d
Vasyldmitrovich <vasyl.dmitrovich@gmail.com>
2024-08-01 22:42:27 +0300

    Change script for creating zip

--- 02_aws/task2_lambda/src/build.sh
+++ 02_aws/task2_lambda/src/build.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 cd ./app
-poetry export -f requirements.txt --output requirements.txt --without-hashes
+#pip install poetry
+#poetry export -f requirements.txt --output requirements.txt --without-hashes
+mkdir package
 pip install --target ./package -r requirements.txt
 cd package
-zip -r9 ../function.zip .
+#zip -r ../function.zip .
+7z a -r ../../function.zip ./*
 cd ../
-zip -g function.zip app.py db.py
+#zip function.zip app.py db.py
+7z a -tzip ../function.zip app.py db.py
