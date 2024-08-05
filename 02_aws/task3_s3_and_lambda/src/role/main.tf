7ca903a364af3f26a5b1829ea3d594841bce0617
Vasyldmitrovich <vasyl.dmitrovich@gmail.com>
2024-08-05 17:54:31 +0300

    Finish role for lambda function

--- 02_aws/task3_s3_and_lambda/src/role/main.tf
+++ 02_aws/task3_s3_and_lambda/src/role/main.tf
@@ -1,26 +1,24 @@
-//resource "aws_iam_role" "lambda_exec" {
-//  name = "lambda_exec_role1"
-//
-//  assume_role_policy = jsonencode({#resourde data data aws_iam_policy_document use
-//    Version = "2012-10-17",
-//    Statement = [{
-//      Action = "sts:AssumeRole",
-//      Effect = "Allow",
-//      Sid    = "",
-//      Principal = {
-//        Service = "lambda.amazonaws.com"
-//      },
-//    }],
-//  })
-//}
+# Create the IAM role with the assume role policy
+resource "aws_iam_role" "s3_sns_lambda" {
+  name = "s3_sns_lambda_role"
 
-//resource "aws_iam_role_policy_attachment" "lambda_exec_policy" {
-//  role       = aws_iam_role.lambda_exec.name
-//  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-//}
+  assume_role_policy = data.aws_iam_policy_document.s3_sns_lambda_role_policy.json
+}
 
-data "aws_iam_policy_document" "s3_sns_lambda" {
+# Define the IAM policy document using the data source
+data "aws_iam_policy_document" "s3_sns_lambda_role_policy" {
   statement {
-    effect = "Allow"
+    actions   = ["sts:AssumeRole"]
+    effect    = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
   }
+}
+
+# Attach the AWS managed policy to the IAM role
+resource "aws_iam_role_policy_attachment" "lambda_exec_policy" {
+  role       = aws_iam_role.s3_sns_lambda.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
--- 02_aws/task3_s3_and_lambda/src/sns/output.tf
+++ 02_aws/task3_s3_and_lambda/src/sns/output.tf
@@ -1,1 +1,3 @@
-#
+output "sns_topic_arn" {
+  value = aws_sns_topic.topic_sns_run_lambda.arn
+}
