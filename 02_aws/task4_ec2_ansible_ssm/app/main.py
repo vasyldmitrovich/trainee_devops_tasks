695eb01fef3dab831165af25791b89b352bfca86
vasyldmitrovich <Rapidvasyaalister@gmail.com>
2024-09-05 13:17:26 +0300

    Add timestamp to seeing whet ssm wil work

--- 02_aws/task4_ec2_ansible_ssm/app/main.py
+++ 02_aws/task4_ec2_ansible_ssm/app/main.py
@@ -1,13 +1,29 @@
 import logging
 import requests
 from calculator import Calculator
+from datetime import datetime
 
+# Configure logging
 logging.basicConfig(level=logging.INFO)
 logging.info("Info logs")
 
+def create_timestamp_file():
+    # Get current timestamp
+    now = datetime.now()
+    timestamp = now.strftime("%Y-%m-%d_%H-%M-%S")
+
+    # Define filename with timestamp
+    filename = f"/home/ubuntu/timestamp_{timestamp}.txt"
+
+    # Write the timestamp to the file
+    with open(filename, "w") as file:
+        file.write(f"Current timestamp: {timestamp}")
 
 def main():
+    # Create a file with the current timestamp
+    create_timestamp_file()
 
+    # Perform calculations
     calc = Calculator()
     result = calc.add(5, 10)
     logging.info(f"The result of addition is: {result}")
@@ -16,3 +32,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
