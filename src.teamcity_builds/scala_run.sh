d34fa0525b7059180c178dca04ded6173e15623b
vasyldmitrovich <Rapidvasyaalister@gmail.com>
2024-10-09 16:21:11 +0300

    Add execution to last script

--- src.teamcity_builds/scala_run.sh
+++ src.teamcity_builds/scala_run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "This is the last build step, without docker container because i do not have it on my laptop"
+echo "Add execution to this file"
 # Run Docker container with Spark
 #docker run -it your-spark-app scala <<EOF
 #:require /opt/spark/jars/your-file.jar
