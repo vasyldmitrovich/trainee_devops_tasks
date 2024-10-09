4378199066e24909a0297684ad99285b8c4efe2f
vasyldmitrovich <Rapidvasyaalister@gmail.com>
2024-10-09 17:36:56 +0300

    A little changes for testing

--- src.teamcity_builds/scala_run.sh
+++ src.teamcity_builds/scala_run.sh
@@ -2,6 +2,7 @@
 
 echo "This is the last build step, without docker container because i do not have it on my laptop"
 echo "Add execution to this file"
+echo "Some little change for seeing automatic build after commit"
 # Run Docker container with Spark
 #docker run -it your-spark-app scala <<EOF
 #:require /opt/spark/jars/your-file.jar
