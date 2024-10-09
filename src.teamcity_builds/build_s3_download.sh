fc50de76e1b75a7fcafbf2dc5b8e79079bbec27d
vasyldmitrovich <Rapidvasyaalister@gmail.com>
2024-10-09 16:01:01 +0300

    Get object from s3 rename of file

--- src.teamcity_builds/build_s3_download.sh
+++ src.teamcity_builds/build_s3_download.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Download JAR from S3
-aws s3api get-object --bucket aws-s3-bucket-for-snd-and-lambda-vbazh1 --key calculator-service-0.0.1-SNAPSHOT.jar
+aws s3api get-object --bucket aws-s3-bucket-for-snd-and-lambda-vbazh1 --key calculator-service-0.0.1-SNAPSHOT.jar my-file.jar
 # Check if the file was downloaded
-if [ -f "./your-file.jar" ]; then
+if [ -f "./my-file.jar" ]; then
     echo "JAR downloaded successfully!"
 else
     echo "Failed to download JAR from S3" >&2
