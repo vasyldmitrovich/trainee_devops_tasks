4da136cc982f89f80a9b21b31a45731f1dfc4af4
vasyldmitrovich <Rapidvasyaalister@gmail.com>
2024-10-09 16:11:33 +0300

    Add key and last build step

--- src.teamcity_builds/scala_run.sh
+++ src.teamcity_builds/scala_run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "This is the last build step"
+echo "This is the last build step, without docker container because i do not have it on my laptop"
 # Run Docker container with Spark
 #docker run -it your-spark-app scala <<EOF
 #:require /opt/spark/jars/your-file.jar
