a9fd92ba7c5a274309b686cb71113230efd0e22d
vasyldmitrovich <Rapidvasyaalister@gmail.com>
2024-10-11 16:22:16 +0300

    Add import

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -2,6 +2,7 @@ import jetbrains.buildServer.configs.kotlin.*
 import jetbrains.buildServer.configs.kotlin.buildFeatures.perfmon
 import jetbrains.buildServer.configs.kotlin.buildSteps.dockerCommand
 import jetbrains.buildServer.configs.kotlin.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.buildSteps.maven
 import jetbrains.buildServer.configs.kotlin.triggers.vcs
 
 /*
