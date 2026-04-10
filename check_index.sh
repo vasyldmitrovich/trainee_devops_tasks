c8c1c9b762465815bc6bb7e335e0940a3df6af27
vasyldmitrovich <48633322+vasyldmitrovich@users.noreply.github.com>
2026-04-10 22:43:00 +0300

    Create check_index.sh

---
+++ check_index.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Перевірка наявності файлу index.html у корені репозиторію
+if [ -f index.html ]; then
+  echo "index.html exists"
+  exit 0
+else
+  echo "index.html does not exist"
+  exit 1
+fi
