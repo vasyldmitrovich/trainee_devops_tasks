10ba93df57ac2c2e5ef911810e779bf122e58367
vasyldmitrovich <48633322+vasyldmitrovich@users.noreply.github.com>
2026-04-10 23:00:45 +0300

    Create check_index.sh into main branch

---
+++ check_index.sh
@@ -0,0 +1,10 @@
+﻿﻿#!/bin/bash
+
+# Перевірка наявності файлу index.html у корені репозиторію
+if [ -f index.html ]; then
+  echo "index.html exists"
+  exit 0
+else
+  echo "index.html does not exist"
+  exit 1
+fi
